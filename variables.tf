06762fb6abaa290b3de985fa47cedbbfe96f4e60
schouta <sriraja_sri@yahoo.com>
2021-11-16 15:15:42 +0000

    Added remote backend

--- variables.tf
+++ variables.tf
@@ -7,11 +7,12 @@
 
 variable "prefix" {
   description = "This prefix will be included in the name of most resources."
+  default = "srikanth"
 }
 
 variable "location" {
   description = "The region where the virtual network is created."
-  default     = "centralus"
+  default     = "uksouth"
 }
 
 variable "address_space" {
